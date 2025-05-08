8a7c3666d2dd743c4aac8b5eace99304128b4f37
guilhermefassis <guilhermefassis-dev@outlook.com>
2025-05-08 09:07:42 -0300

    model Order and Validation tables and create Repository layer in project

---
+++ product-validation-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO root.product(code) VALUES('COMIC_BOOKS');
+INSERT INTO root.product(code) VALUES('BOOKS');
+INSERT INTO root.product(code) VALUES('MOVIES');
+INSERT INTO root.product( code) VALUES('MUSIC');
