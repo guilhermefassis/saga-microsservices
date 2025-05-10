81269633ad97b050815d2f77648b6e28a7d09cb3
guilhermefassis <guilhermefassis-dev@outlook.com>
2025-05-10 13:39:20 -0300

    create model and repository to payment service.

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/model/Payment.java
@@ -0,0 +1,50 @@
+package br.com.microservices.orchestrated.paymentservice.core.model;
+
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String orderId;
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private int totalItems;
+    @Column(nullable = false)
+    private double totalAmount;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EPaymentStatus status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.status = EPaymentStatus.PENDING;
+        var now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
