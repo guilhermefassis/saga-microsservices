33c40a844e23c7d15118414ceb100c523bb89a9f
guilhermefassis <guilhermefassis-dev@outlook.com>
2025-05-12 20:55:43 -0300

    create model and repository to inventory

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -0,0 +1,55 @@
+package br.com.microservices.orchestrated.inventoryservice.core.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orderInventory")
+public class OrderInventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id", nullable = false)
+    private Inventory inventory;
+    @Column(nullable = false)
+    private Integer orderQuantity;
+    @Column(nullable = false)
+    private Integer oldQuantity;
+    @Column(nullable = false)
+    private Integer newQuantity;
+
+    @Column(nullable = false)
+    private String orderId;
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ inventory-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO root.inventory(product_code, available) VALUES('COMIC_BOOKS', 4);
+INSERT INTO root.inventory(product_code, available) VALUES('BOOKS', 2);
+INSERT INTO root.inventory(product_code, available) VALUES('MOVIES', 5);
+INSERT INTO root.inventory(product_code, available) VALUES('MUSIC', 9);
